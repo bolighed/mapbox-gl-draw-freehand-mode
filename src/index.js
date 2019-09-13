9a7151e80e8f53decab7d38cc3632629c30d21cd
Rasmus Kildevæld <rasmuskildevaeld@gmail.com>
2019-09-13 11:03:02 +0200

    wrap simplification in try catch, because it sometimes break on touch devices

--- src/index.js
+++ src/index.js
@@ -1,70 +1,74 @@
-import doubleClickZoom from '@mapbox/mapbox-gl-draw/src/lib/double_click_zoom';
 import Constants from '@mapbox/mapbox-gl-draw/src/constants';
+import doubleClickZoom from '@mapbox/mapbox-gl-draw/src/lib/double_click_zoom';
 import DrawPolygon from '@mapbox/mapbox-gl-draw/src/modes/draw_polygon';
-import dragPan from '../src/lib/drag_pan';
-import simplify from "@turf/simplify";
+import simplify from '@turf/simplify';
+import clone from '@turf/clone';
 
+import dragPan from '../src/lib/drag_pan';
 const FreeDraw = DrawPolygon;
 
-FreeDraw.onSetup = function() {
-    const polygon = this.newFeature({
-        type: Constants.geojsonTypes.FEATURE,
-        properties: {},
-        geometry: {
-            type: Constants.geojsonTypes.POLYGON,
-            coordinates: [[]]
-        }
-    });
 
-    this.addFeature(polygon);
+FreeDraw.onSetup = function () {
+  const polygon = this.newFeature({
+    type: Constants.geojsonTypes.FEATURE,
+    properties: {},
+    geometry: { type: Constants.geojsonTypes.POLYGON, coordinates: [[]] }
+  });
 
-    this.clearSelectedFeatures();
-    doubleClickZoom.disable(this);
-    dragPan.disable(this);
-    this.updateUIClasses({ mouse: Constants.cursors.ADD });
-    this.activateUIButton(Constants.types.POLYGON);
-    this.setActionableState({
-        trash: true
-    });
+  this.addFeature(polygon);
 
-    return {
-        polygon,
-        currentVertexPosition: 0,
-        dragMoving: false
-    };
+  this.clearSelectedFeatures();
+  doubleClickZoom.disable(this);
+  dragPan.disable(this);
+  this.updateUIClasses({ mouse: Constants.cursors.ADD });
+  this.activateUIButton(Constants.types.POLYGON);
+  this.setActionableState({ trash: true });
+
+  return { polygon, currentVertexPosition: 0, dragMoving: false };
 };
 
-FreeDraw.onDrag = FreeDraw.onTouchMove = function (state, e){
+FreeDraw.onDrag = FreeDraw.onTouchMove =
+  function (state, e) {
     state.dragMoving = true;
     this.updateUIClasses({ mouse: Constants.cursors.ADD });
-    state.polygon.updateCoordinate(`0.${state.currentVertexPosition}`, e.lngLat.lng, e.lngLat.lat);
+    state.polygon.updateCoordinate(
+      `0.${state.currentVertexPosition}`, e.lngLat.lng, e.lngLat.lat);
     state.currentVertexPosition++;
-    state.polygon.updateCoordinate(`0.${state.currentVertexPosition}`, e.lngLat.lng, e.lngLat.lat);
-}
+    state.polygon.updateCoordinate(
+      `0.${state.currentVertexPosition}`, e.lngLat.lng, e.lngLat.lat);
+  }
 
-FreeDraw.onMouseUp = function (state, e){
+
+FreeDraw.onMouseUp =
+  function (state, e) {
+
     if (state.dragMoving) {
-        var tolerance = (3 / ((this.map.getZoom()-4) * 150)) - 0.001 // https://www.desmos.com/calculator/b3zi8jqskw
-        simplify(state.polygon, {
-            mutate: true,
-            tolerance: tolerance,
-            highQuality: true
-        });
-            
-        this.fireUpdate();
-        this.changeMode(Constants.modes.SIMPLE_SELECT, { featureIds: [state.polygon.id] });
+      const polygon = clone(state.polygon),
+        tolerance = (3 / ((this.map.getZoom() - 4) * 150)) -
+          0.001  // https://www.desmos.com/calculator/b3zi8jqskw
+
+      try {
+        simplify(
+          polygon, { mutate: true, tolerance: tolerance, highQuality: true });
+        state.polygon = polygon;
+      } catch (_e) { }
+
+      this.fireUpdate();
+      this.changeMode(
+        Constants.modes.SIMPLE_SELECT, { featureIds: [state.polygon.id] });
     }
-}
+  }
 
-FreeDraw.onTouchEnd = function(state, e) {
+FreeDraw.onTouchEnd =
+  function (state, e) {
     this.onMouseUp(state, e)
-}
+  }
 
-FreeDraw.fireUpdate = function() {
-    this.map.fire(Constants.events.UPDATE, {
-        action: Constants.updateActions.MOVE,
-        features: this.getSelected().map(f => f.toGeoJSON())
-    });
+FreeDraw.fireUpdate = function () {
+  this.map.fire(Constants.events.UPDATE, {
+    action: Constants.updateActions.MOVE,
+    features: this.getSelected().map(f => f.toGeoJSON())
+  });
 };
 
 export default FreeDraw
